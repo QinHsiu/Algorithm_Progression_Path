3def890777eaac8aad5bc463a16e7360fabe6e20
xiuyuan.qin <xiuyuan.qin@mobvoi.com>
2023-05-19 02:10:45 +0000

    update

--- easy_code.py
+++ easy_code.py
@@ -18,16 +18,26 @@ def bubble_sort(arr,mode):
         sortBorder=lastExchangeIndex
         if flag:
             break
-   
-        
-        
+    return arr
+
+def selection_sort(arr):
+    # 简单选择排序，每次选择两个交换位置
+    arr=copy.deepcopy(arr)
+    n=len(arr)
+    for i in range(n-1):
+        mark=i
+        for j in range(i+1,n):
+            if arr[j]<arr[mark]:
+                mark=j
+        if mark!=i:
+            arr[i],arr[mark]=arr[mark],arr[i]
     return arr
 
 
         
 if __name__=="__main__":
     test_=[1,2,4,5,3]
-    print(test_,bubble_sort(test_))
-    
+    # print(test_,bubble_sort(test_))
+    print(test_,selection_sort(test_))
     
 
